46794613c1d9996be6618c2b25733b8d31c9b102
Bram <baimz.bramz@gmail.com>
2024-05-27 16:54:49 +0700

    "React Apps"

---
+++ src/components/Card.jsx
@@ -0,0 +1,10 @@
+// eslint-disable-next-line react/prop-types
+const Card = ({ children, bg }) => {
+  return (
+    <>
+      <div className={`${bg} p-6 rounded-lg shadow-md`}>{children}</div>
+    </>
+  );
+};
+
+export default Card;
