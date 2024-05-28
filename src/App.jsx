734aaba673adf90caaf26b982edb5abe12919a9b
Bram <baimz.bramz@gmail.com>
2024-05-28 16:24:34 +0700

    "Detail Page"

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,14 @@ import HomePage from "./pages/HomePage";
 import MainLayout from "./layouts/MainLayout";
 import JobsPage from "./pages/JobsPage";
 import ErrorPage from "./pages/ErrorPage";
+import DetailPage from "./pages/DetailPage";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<MainLayout />}>
       <Route index element={<HomePage />} />
       <Route path="/jobs" element={<JobsPage />} />
+      <Route path="/detail/:id" element={<DetailPage />} />
       <Route path="*" element={<ErrorPage />} />
     </Route>
   )
--- src/components/JobList.jsx
+++ src/components/JobList.jsx
@@ -37,7 +37,7 @@ const JobList = ({ jobs }) => {
             {jobs.location}
           </div>
           <Link
-            to={`/jobs/${jobs.id}`}
+            to={`/detail/${jobs.id}`}
             className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
           >
             Read More
