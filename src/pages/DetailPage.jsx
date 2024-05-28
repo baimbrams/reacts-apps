54f0efa59b61096520666e5a7f87119415fecee9
Bram <baimz.bramz@gmail.com>
2024-05-28 16:38:55 +0700

    "detail page 2:13:13"

--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -1,5 +1,28 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import Spinner from "../components/Spinner";
+
 const DetailPage = () => {
-  return <div>DetailPage</div>;
+  const { id } = useParams();
+  const [jobs, setJobs] = useState([null]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchJob = async () => {
+      try {
+        const response = await fetch(`/api/jobs/${id}`);
+        const data = await response.json();
+        setJobs(data);
+      } catch (error) {
+        console.error("erorr woy", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchJob();
+  }, [id]);
+
+  return loading ? <Spinner /> : <h1>{jobs.title}</h1>;
 };
 
 export default DetailPage;
